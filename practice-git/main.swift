fc1edf57506c7b8a3691f8700432725c2ea6edf5
sneric <sneric.smith@gmail.com>
2019-05-05 10:27:34 -0500

    Initial commit

---
+++ practice-git/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  practice-git
+//
+//  Created by Eric Smith on 5/4/19.
+//  Copyright © 2019 Eric Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
